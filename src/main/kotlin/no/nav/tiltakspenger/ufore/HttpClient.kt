52fbaae43a91ce6ada160a9495cdb9fbe5639d60
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-12-08 21:47:55 +0100

    Handle non 2xx-responses in ktor client, part 2

--- src/main/kotlin/no/nav/tiltakspenger/ufore/HttpClient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/ufore/HttpClient.kt
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
+import io.ktor.http.*
 import io.ktor.serialization.jackson.*
+import mu.KotlinLogging
 
 class HttpClient(config: CIOEngineConfig.() -> Unit = {}) {
+    private val log = KotlinLogging.logger {}
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             jackson {
@@ -19,6 +23,16 @@ class HttpClient(config: CIOEngineConfig.() -> Unit = {}) {
             }
         }
         expectSuccess = true
+        HttpResponseValidator {
+            handleResponseExceptionWithRequest { exception, _ ->
+                val clientException = exception as? ClientRequestException ?: return@handleResponseExceptionWithRequest
+                val exceptionResponse = clientException.response
+                if (exceptionResponse.status == HttpStatusCode.BadRequest) {
+                    log.error { "400 mot pesys: $exceptionResponse" }
+                    // TODO: handle this
+                }
+            }
+        }
         install(Logging) {
             level = LogLevel.INFO
         }
