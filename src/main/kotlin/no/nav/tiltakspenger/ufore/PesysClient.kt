d02c0875b12666862999570106b60e527fe0befd
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-12-10 22:23:49 +0100

    Use expression body instead of block body, just because (not sure if it is more readable, though)

--- src/main/kotlin/no/nav/tiltakspenger/ufore/PesysClient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/ufore/PesysClient.kt
@@ -9,28 +9,24 @@ import java.time.LocalDate
 
 class PesysClient(private val client: HttpClient, private val getToken: suspend () -> String) {
     private val config = Configuration.PesysConfig()
-    suspend fun hentUføre(ident: String, fom: String, tom: String, behovId: String): UføreResponse {
-        val token = getToken()
-        val response: UføreResponse = try {
-            client.get(urlString = config.pesysUføreUrl) {
-                bearerAuth(token)
-                contentType(ContentType.Application.Json)
-                accept(ContentType.Application.Json)
-                header("Nav-Call-Id", behovId)
-                header("fnr", ident)
-                url {
-                    parameters.append("fom", fom)
-                    parameters.append("tom", tom)
-                    parameters.append("uforeTyper", "UFORE")
-                    parameters.append("uforeTyper", "UF_M_YRKE")
-                }
-            }.body()
-        } catch (e: ClientRequestException) {
-            if (e.response.status == HttpStatusCode.NotFound) {
-                UføreResponse(false, null, null)
-            } else throw (e)
-        }
-        return response
+    suspend fun hentUføre(ident: String, fom: String, tom: String, behovId: String): UføreResponse = try {
+        client.get(urlString = config.pesysUføreUrl) {
+            bearerAuth(getToken())
+            contentType(ContentType.Application.Json)
+            accept(ContentType.Application.Json)
+            header("Nav-Call-Id", behovId)
+            header("fnr", ident)
+            url {
+                parameters.append("fom", fom)
+                parameters.append("tom", tom)
+                parameters.append("uforeTyper", "UFORE")
+                parameters.append("uforeTyper", "UF_M_YRKE")
+            }
+        }.body()
+    } catch (e: ClientRequestException) {
+        if (e.response.status == HttpStatusCode.NotFound) {
+            UføreResponse(false, null, null)
+        } else throw (e)
     }
 }
 
