c8b27f3476b24115693982c8ee7004e4c29a01ee
Per Jørgen Walstrøm <per.jorgen.walstrom@nav.no>
2022-12-10 01:39:26 +0100

    Add some logging

--- src/main/kotlin/no/nav/tiltakspenger/ufore/HttpClient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/ufore/HttpClient.kt
@@ -9,9 +9,12 @@ import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.serialization.jackson.*
+import mu.KotlinLogging
 
+val LOG = KotlinLogging.logger {}
 fun httpClient(engine: HttpClientEngine = CIO.create(), config: HttpClientEngineConfig.() -> Unit = {}) =
     HttpClient(engine) {
+        LOG.info { "Setting up httpclient with config: $config" }
         install(ContentNegotiation) {
             jackson {
                 configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
@@ -24,4 +27,5 @@ fun httpClient(engine: HttpClientEngine = CIO.create(), config: HttpClientEngine
             level = LogLevel.INFO
         }
         engine(config)
+        LOG.info { "Returning httpclient $this" }
     }
