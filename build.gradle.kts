3eee989629c95e94ddc2e7d1c34d456e9cbe3ec8
Hågen Hasle <haagenhasle@gmail.com>
2023-11-29 21:12:16 +0100

    R Oppgradert til Gradle 8.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val felleslibVersion = "0.0.51"
 plugins {
     application
     kotlin("jvm") version "1.9.21"
-    id("com.diffplug.spotless") version "6.21.0"
+    id("com.diffplug.spotless") version "6.23.1"
     // id("ca.cutterslade.analyze") version "1.9.1"
 }
 
